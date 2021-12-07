908bdfcc0c87e182d6576880174474d74a3a275a
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-07 15:26:14 +0100

    Types and variables

---
+++ Topics/Types and variables/Assignments (1)/src/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Assign values to these four variables
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int four = 4;
+
+        System.out.printf("%d %d %d %d%n", one, two, three, four);
+    }
+}
