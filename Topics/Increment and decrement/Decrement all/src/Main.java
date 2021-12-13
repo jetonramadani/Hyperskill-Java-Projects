1889d561d6295bbdef0ab57fa0c8b2cf10e106c7
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-13 14:29:20 +0100

    Increment and Decrement

---
+++ Topics/Increment and decrement/Decrement all/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int num = scanner.nextInt();
+        System.out.print(--num + " ");
+        num = scanner.nextInt();
+        System.out.print(--num + " ");
+        num = scanner.nextInt();
+        System.out.print(--num + " ");
+        num = scanner.nextInt();
+        System.out.print(--num);
+    }
+}
