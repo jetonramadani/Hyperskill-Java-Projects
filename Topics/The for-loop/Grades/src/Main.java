74fd6eb19870e0909771375aaacd31dec76fcb33
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-03-07 09:06:50 +0100

    Boxing and unboxing

--- Topics/The for-loop/Grades/src/Main.java
+++ Topics/The for-loop/Grades/src/Main.java
@@ -4,37 +4,6 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
-        int n = scanner.nextInt();
 
-        int fives = 0;
-        int fours = 0;
-        int threes = 0;
-        int twos = 0;
-
-        final int five = 5;
-        final int four = 4;
-        final int three = 3;
-        final int two = 2;
-
-        for (int i = 0; i < n; i++) {
-            int num = scanner.nextInt();
-            switch (num) {
-                case five:
-                    ++fives;
-                    break;
-                case four:
-                    ++fours;
-                    break;
-                case three:
-                    ++threes;
-                    break;
-                case two:
-                    ++twos;
-                    break;
-                default:
-                    break;
-            }
-        }
-        System.out.printf("%d %d %d %d", twos, threes, fours, fives);
     }
 }
