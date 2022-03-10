737d8f731f3dc794505c5b88a9336919ca15897f
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-03-10 15:44:36 +0100

    Optional

---
+++ Topics/Optional/Check an Optional value/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Optional;
+
+class Main {
+    public static void main(String[] args) {
+        ValueProvider provider = new ValueProvider();
+        // use provider.getValue() to get Optional<String>
+        final Optional<String> res = provider.getValue();
+        if (res.isPresent()) {
+            System.out.println(res.get());
+        }
+
+    }
+}
+
+class ValueProvider {
+    private Optional<String> inputOpt; // cache to provide reproducing method invocation
+
+    public Optional<String> getValue() {
+        if (inputOpt == null) {
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            String input = scanner.next();
+            inputOpt = "null".equals(input) ? Optional.empty() : Optional.of(input);
+        }
+
+        return inputOpt;
+    }
+}
