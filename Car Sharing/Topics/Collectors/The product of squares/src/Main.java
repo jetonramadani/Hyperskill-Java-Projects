222892efeda3a3bbe59b23c64fa379734b2bd70e
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-03-13 13:51:39 +0100

    Collectors

---
+++ Car Sharing/Topics/Collectors/The product of squares/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class CollectorProduct {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] values = scanner.nextLine().split(" ");
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String val : values) {
+            numbers.add(Integer.parseInt(val));
+        }
+
+        long val = numbers.stream().reduce(1, (res, num) -> res * num * num);
+
+        System.out.println(val);
+    }
+}
