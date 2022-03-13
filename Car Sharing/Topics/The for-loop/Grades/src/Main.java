e430eb81766a09ff5072e35058027abe56cef207
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-03-13 12:53:17 +0100

    The foor loop

--- Car Sharing/Topics/The for-loop/Grades/src/Main.java
+++ Car Sharing/Topics/The for-loop/Grades/src/Main.java
@@ -1,9 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
+        HashMap<String, Integer> grades = new HashMap<>() {
+            {
+                put("D", 0);
+                put("C", 0);
+                put("B", 0);
+                put("A", 0);
+            }
+        };
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String grade = scanner.nextLine();
+            grades.computeIfPresent(grade, (s, integer) -> integer + 1);
+        }
+        List<String> res = grades
+                .values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.toList());
+        Collections.reverse(res);
+        System.out.println(String.join(" ", res));
 
     }
 }
