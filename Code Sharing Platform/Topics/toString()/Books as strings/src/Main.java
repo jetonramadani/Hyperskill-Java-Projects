26a2f844c83f7c5fc794b53fbf1045030f2621df
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-03-22 10:59:49 +0100

    toString()

---
+++ Code Sharing Platform/Topics/toString()/Books as strings/src/Main.java
@@ -0,0 +1,23 @@
+
+class Book {
+
+    private String title;
+    private int yearOfPublishing;
+    private String[] authors;
+
+    public Book(String title, int yearOfPublishing, String[] authors) {
+        this.title = title;
+        this.yearOfPublishing = yearOfPublishing;
+        this.authors = authors.clone();
+    }
+
+    @Override
+    public String toString() {
+        return
+                "title=" + title +
+                        ",yearOfPublishing=" + yearOfPublishing +
+                        ",authors=[" + String.join(",", authors) +
+                        "]";
+    }
+
+}
