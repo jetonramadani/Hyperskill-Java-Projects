e23ae5cd0f5c55b3c7ff3387e67abda734e1cdec
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-04-19 14:44:23 +0200

    Project reload

--- Code Sharing Platform/Topics (1)/Threads as objects/Is thread daemon/src/Main.java
+++
@@ -1,6 +0,0 @@
-class ThreadUtil {
-    public static void printIfDaemon(Thread thread) {
-        // implement logic
-        System.out.println(thread.isDaemon() ? "daemon" : "not daemon");
-    }
-}
--- Code Sharing Platform/Topics (1)/Threads as objects/Thread name/src/Main.java
+++
@@ -1,7 +0,0 @@
-class ThreadUtil {
-    public static String getName(Thread thread) {
-        // implement logic
-        return thread.getName();
-    }
-
-}
