02d273a6f4be4a94a30f9d8caca613ed0dea17e4
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-07-05 10:24:39 +0200

    Cinema Rom Manager Stage 3/5: Tickets

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -9,6 +9,8 @@ public class Cinema {
         private final char[][] seats;
         private final int rows;
         private final int seatsPerRow;
+        private final boolean isAll10USD;
+        private final int firstHalf;
 
         public Cino() {
             System.out.println("Enter the number of rows:");
@@ -21,26 +23,42 @@ public class Cinema {
                     seats[i][j] = 'S';
                 }
             }
+            this.isAll10USD = this.rows * this.seatsPerRow <= 60;
+            this.firstHalf = Math.floorDiv(this.rows, 2);
         }
 
         public void printTotal() {
             System.out.println("Total income:");
             int income = 0;
-            if (rows * seatsPerRow <= 60) {
-                income = rows * seatsPerRow * 10;
+            if (isAll10USD) {
+                income = this.rows * this.seatsPerRow * 10;
             } else {
-                int firstHalf = Math.floorDiv(rows, 2);
-                int secondHalf = rows - firstHalf;
-                income += firstHalf * seatsPerRow * 10;
-                income += secondHalf * seatsPerRow * 8;
+                int secondHalf = this.rows - this.firstHalf;
+                income += this.firstHalf * this.seatsPerRow * 10;
+                income += secondHalf * this.seatsPerRow * 8;
             }
-            System.out.printf("$%d", income);
+            System.out.printf("$%d%n", income);
+        }
+
+        public void bookSeat() {
+            System.out.println("Enter a row number:");
+            int seatRow = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            int seatColumn = scanner.nextInt();
+            seats[seatRow - 1][seatColumn - 1] = 'B';
+            int price = 0;
+            if (isAll10USD) {
+                price = 10;
+            } else {
+                price = seatRow <= firstHalf ? 10 : 8;
+            }
+            System.out.printf("Ticket price: $%d%n", price);
         }
 
         public void printCino() {
             // Write your code here
             System.out.printf("Cinema:%n ");
-            for (int i = 1; i <= this.rows; i++) {
+            for (int i = 1; i <= this.seatsPerRow; i++) {
                 System.out.printf(" %d", i);
             }
             for (int i = 0; i < this.rows; i++) {
@@ -49,12 +67,15 @@ public class Cinema {
                     System.out.printf(" %c", this.seats[i][j]);
                 }
             }
+            System.out.println();
         }
     }
 
 
     public static void main(String[] args) {
         Cino cinema = new Cino();
-        cinema.printTotal();
+        cinema.printCino();
+        cinema.bookSeat();
+        cinema.printCino();
     }
 }
