30ebd63756761756733da3bade664a3bce61d4b2
Jeton.Ramadani <jeton.ramadani@students.finki.ukim.mk>
2022-07-05 10:12:15 +0200

    Cinema Rom Manager Stage 2/5: Sold

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,27 +1,60 @@
 package cinema;
 
-public class Cinema {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        // Write your code here
-        char[][] seats = {
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-                {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'}
-        };
-        System.out.printf("Cinema:%n ");
-        for (int i = 1; i <= seats[0].length; i++) {
-            System.out.printf(" %d", i);
-        }
-        for (int i = 0; i < seats.length; i++) {
-            System.out.printf("%n%d", i + 1);
-            for (int j = 0; j < seats[i].length; j++) {
-                System.out.printf(" %c", seats[i][j]);
+public class Cinema {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static class Cino {
+        private final char[][] seats;
+        private final int rows;
+        private final int seatsPerRow;
+
+        public Cino() {
+            System.out.println("Enter the number of rows:");
+            this.rows = scanner.nextInt();
+            System.out.println("Enter the number of seats in each row:");
+            this.seatsPerRow = scanner.nextInt();
+            seats = new char[rows][seatsPerRow];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < seatsPerRow; j++) {
+                    seats[i][j] = 'S';
+                }
             }
         }
+
+        public void printTotal() {
+            System.out.println("Total income:");
+            int income = 0;
+            if (rows * seatsPerRow <= 60) {
+                income = rows * seatsPerRow * 10;
+            } else {
+                int firstHalf = Math.floorDiv(rows, 2);
+                int secondHalf = rows - firstHalf;
+                income += firstHalf * seatsPerRow * 10;
+                income += secondHalf * seatsPerRow * 8;
+            }
+            System.out.printf("$%d", income);
+        }
+
+        public void printCino() {
+            // Write your code here
+            System.out.printf("Cinema:%n ");
+            for (int i = 1; i <= this.rows; i++) {
+                System.out.printf(" %d", i);
+            }
+            for (int i = 0; i < this.rows; i++) {
+                System.out.printf("%n%d", i + 1);
+                for (int j = 0; j < this.seatsPerRow; j++) {
+                    System.out.printf(" %c", this.seats[i][j]);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Cino cinema = new Cino();
+        cinema.printTotal();
     }
 }
